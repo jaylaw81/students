c6b787fe2ec66704ea828f43013f59a0d808eaed
Jason Law <jlaw@sapient.com>
2015-01-28 18:48:09 -0500

    test auto deploy

--- app/public/js/views/editor.js
+++ app/public/js/views/editor.js
@@ -10,7 +10,7 @@ rte = {
         this.rteRender();
         this.events();
         //this.autoSave();
-        console.log('init');
+        console.log('init new');
     },
 
     getTasks: function(type){
