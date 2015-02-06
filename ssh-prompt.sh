01710a4eaeff7a9308865aa0c183c30064a8dd6a
Jason Law <jlaw@sapient.com>
2015-02-06 14:58:39 -0500

    removed bad directories

--- ssh-prompt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/expect -r
-# set variables
-set password software
-spawn git push
-# Look for password prompt
-expect "'/home/ec2-user/.ssh/id_rsa':"
-# Send password aka $password
-send -- "$password\r"
-expect eof
