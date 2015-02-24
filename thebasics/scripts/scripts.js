172d5c8ccdd1fbb9a674bbe670a5face57222119
rhinocoders <info@rhinocoders>
2015-02-24 20:26:17 +0000

    Commit work for jlaw @ 2015:02:24:20:26:17

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -4,3 +4,5 @@ var stringPartNumbers = "numbers";
 var stringPartCharacters = "characters";
 var myFullString = "A string may contain as many " stringPartLetters ", " stringPartNumbers " and " stringPartCharacters " as you want.";
 document.querySelector("#my-string").innerHTML = myFullString;
+var count = 10;
+var multiplier = 20;
