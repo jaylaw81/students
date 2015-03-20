6a204b7d230c2c563d18b7b58fd8bd3810345220
rhinocoders <info@rhinocoders>
2015-03-20 12:53:11 +0000

    Commit work for matthew @ 2015:03:20:12:53:11

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -1,6 +1,1 @@
-var myVariable = "value";
-var stringPartLetters = "letters";
-var stringPartNumbers = "numbers";
-var stringPartCharacters = "characters";
-
-"I have "+letters+", "+numbers+" and "+characters+" all part of my string";
+var string = "I have "+letters+", "+numbers+" and "+characters+" all part of my string";
