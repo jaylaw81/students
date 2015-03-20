fdc0cbf09c2447dac85a45dfee793ce7f6502e7e
rhinocoders <info@rhinocoders>
2015-03-20 16:16:05 +0000

    Commit work for jlaw @ 2015:03:20:16:16:05

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -1,13 +1,36 @@
-var myVariable = "value33";
+var myVariable = "value";
 var stringPartLetters = "letters";
 var stringPartNumbers = "numbers";
 var stringPartCharacters = "characters";
-var myFullString = "A string may contain as many "+stringPartLetters+", "+stringPartNumbers+" and "+stringPartCharacters+" as you want.";
+
+var myFullString = "A string may contain as many "+stringPartLetters+" "+stringPartNumbers+" "+stringPartCharacters+" asdf.";
+
 console.log(myFullString);
+
 var count = 10;
 var multiplier = 20;
 
-
-var calculateVariables = "I have "+(count * multiplier)+" friends on 'facebook'.";
+var calculateVariables = "I have "+(count * multiplier)+" friends on 'facebook'";
 console.log(calculateVariables);
-console.log('Hello There!');
+
+var stringYear = "In the year "+multiplier.toString() + count.toString()+", most of us were over 3 years old.";
+console.log(stringYear);
+
+var myColorArray = ['Orange','Red','Yellow','Green'];
+
+var myColor = "My favorite color is " + myColorArray[2];
+console.log(myColor);
+
+var myFruitColorArray = [['Apples','Bannanas','Grapes'], ['Red','Yellow','Green']];
+
+var myFruitColor = "I like "+myFruitColorArray[0][1]+" because they are "+myFruitColorArray[1][1];
+console.log(myFruitColor);
+
+var gemString = "Gem colors: ";
+var myGemColorArray = [['Ruby','Saphire','Emerald'], ['Red','Blue','Green']];
+for(var i = 0; i < myGemColorArray[0].length; i++){
+gemString += "The "+myGemColorArray[0][i]+" is "+myGemColorArray[1][i]+".";
+gemString += "Gems come in many colors.";
+}
+
+console.log(gemString);
