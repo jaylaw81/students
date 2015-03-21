02c28181098862aba20e735f66c3869386815358
rhinocoders <info@rhinocoders>
2015-03-21 16:36:20 +0000

    Commit work for NikolasT @ 2015:03:21:16:36:20

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -62,4 +62,5 @@ function createGallery(element,path,file,alt)
         liString += '<li><a href="#self" onclick="showImage(\'' + file + j + '.jpg\');"><img class="photo" alt="' + alt + '-' + j + '" src="' + path + file + j + '.jpg"></a></li>\n';
         }
         document.querySelector(element).innerHTML = liString;
+        var galleryContainer = ".photo-gallery";
     }
