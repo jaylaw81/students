c9e0bd4258da379d0429e9e619d5d307d9001768
rhinocoders <info@rhinocoders>
2015-03-21 13:38:09 +0000

    Commit work for Trevcar7 @ 2015:03:21:13:38:09

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -4,3 +4,5 @@ var stringpartnumbers = 'numbers';
 var stringpartcharacters = 'characters';
 var myfullstring = ' A string may contain as many '+stringpartletters+', '+stringpart+' and '+stringpartcharacters+' as you want.';
 console.log(myFullstring);
+var count = 10
+var multiplier = 20;
