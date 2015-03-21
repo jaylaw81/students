3775ae6b3b1034bc3925692251542fd6b67fae25
rhinocoders <info@rhinocoders>
2015-03-21 14:55:18 +0000

    Commit work for hermione1217 @ 2015:03:21:14:55:18

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -18,6 +18,4 @@ var myFruitColor = "I like "+myFCA[0][2]+" because they are "+myFCA[1][2];
 console.log(myFruitColor);
 var gemString = "Gem colors: ";
 var myGemColorArray = [ ['Ruby', 'Sapphire', 'Emerald'], ['Red', 'Blue', 'Green'] ];
-for(var i = 0; i < myGemColorArray[0].length; i++){};
-var gemString = "The " + myGemColorArray[0][i] +" is "+ myGemColorArray[1][i] + ". ";
-console.log(gemString);
+gemString += "The " + myGemColorArray[0][i] +" is "+ myGemColorArray[1][i] + ". ";
