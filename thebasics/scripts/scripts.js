b5af03e78ec39c522475231a44a444099b379880
rhinocoders <info@rhinocoders>
2015-03-21 14:03:23 +0000

    Commit work for hermione1217 @ 2015:03:21:14:03:23

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -12,4 +12,4 @@ console.log(calcVar);
 var stringYear = "In the year " + multiplier.toString() + count.toString() + ", most of us were over 3 years old.";
 console.log(stringYear);
 var myColorArray = ['Orange', 'Red', 'Yellow', 'Green'];
-console.log(myColorArray);
+var myColor = "My favorite color is " + myColorArray[2];
