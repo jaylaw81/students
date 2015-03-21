3121a016dd9cddc6a4d880c66554473be1bd1852
rhinocoders <info@rhinocoders>
2015-03-21 16:35:53 +0000

    Commit work for jaidenray @ 2015:03:21:16:35:53

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -40,5 +40,7 @@ function myFirstFunction( _myFriend, _myName, _status ) {
 myFirstFunction('ice','jaiden','first');
 function createGallery(element,path,file,alt){ 
     var liString = "";
-    for (var j = 1; j <= 8; j++) { }
+    for (var j = 1; j <= 8; j++) {
+        liString += '<li><a href="#self" onclick="showImage(\'' + file + j + '.jpg\');"><img class="photo" alt="' + alt + '-' + j + '" src="' + path + file + j + '.jpg"></a></li>\n';
+}
 }
