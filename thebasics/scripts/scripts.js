cc7bf8e95a343ce6f14ba3e8fd120edf86573f20
rhinocoders <info@rhinocoders>
2015-03-21 17:48:22 +0000

    Commit work for Trevcar7 @ 2015:03:21:17:48:22

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -41,6 +41,7 @@ function myFirstFunction(_myFriend, _myName, _status )
     console.log("Hello "+_myFriend +". It is me, "+ _myName +" and this is my "+ _status +" function!"); 
 }
 myFirstFunction('Mike','Ben','first');
+
 function createGallery(element,path,file,alt){
     var liString = "";
     for (var j = 1; j <=8; j++) {
