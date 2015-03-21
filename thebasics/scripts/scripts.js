0ba69ace712243cb6a038eb6f432529725647382
rhinocoders <info@rhinocoders>
2015-03-21 00:36:12 +0000

    Commit work for Jon2 @ 2015:03:21:00:36:12

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -19,5 +19,6 @@ console.log(myFruitColor);
 var gemString = "Gem Colors: ";
 var myGemColorArray = [ ['Ruby', 'Sapphire', 'Emerald'], ['Red', 'Blue', 'Green'] ];
 for(var i = 0; i < myGemColorArray[0].length; i++){
-    gemstring += "The " + myGemColorArray[0][i] +" is "+mygemColorArray[1][i]+ ".";    };
-    gemstring += "Gems come in many colors.";
+    gemString += "The " + myGemColorArray[0][i] +" is "+mygemColorArray[1][i]+ ".";    };
+    gemString += "Gems come in many colors.";
+    console.log(gemString);
