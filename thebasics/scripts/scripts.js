441026c09d2418ce04c76f0614d741e3fe378f8f
rhinocoders <info@rhinocoders>
2015-03-21 00:39:13 +0000

    Commit work for Jon2 @ 2015:03:21:00:39:13

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -22,3 +22,5 @@ for(var i = 0; i < myGemColorArray[0].length; i++){
     gemString += "The " + myGemColorArray[0][i] +" is "+myGemColorArray[1][i]+ ".";    };
     gemString += "Gems come in many colors.";
     console.log(gemString);
+var myFriendCompare = "";
+var myFriendName = "Mike";
