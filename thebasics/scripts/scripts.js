384a1b905e59241bf519ab85c9baacd4efa7d943
rhinocoders <info@rhinocoders>
2015-03-21 17:41:37 +0000

    Commit work for bendoliver @ 2015:03:21:17:41:37

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -64,6 +64,7 @@ createGallery(galleryContainer, imagePath, imageName, altName);
 function showImage(argument){
     var img = "<img src='/animals/lion/"+argument+"' />";
     document.querySelector(".gallery-main-image .image-container").innerHTML = img;  
+    return false;
 };
 
 
