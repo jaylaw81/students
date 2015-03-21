2a17935cdac02b0df1b885b123b0da5e2e318170
rhinocoders <info@rhinocoders>
2015-03-21 17:33:20 +0000

    Commit work for natehg24 @ 2015:03:21:17:33:20

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -58,8 +58,9 @@ var altName = "germanshepherd";
 
 
 createGallery(galleryContainer, imagePath, imageName, altName);
+
 function showImage(argument) { 
-    var img = "<img src='/amimals/germanshepherd/" + argument + ".jpg'>";
+    var img = "<img src='/animals/germanshepherd/" + argument + "'/>";
     document.querySelector(".gallery-main-image .image-container").innerHTML = img;
     return false;
 };
