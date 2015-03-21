30736e5ae159ea78ba0586c71ae84d3e14c0637d
rhinocoders <info@rhinocoders>
2015-03-21 15:08:43 +0000

    Commit work for michael345 @ 2015:03:21:15:08:43

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -22,3 +22,4 @@ for(var i = 0; i < myGemColorArray[0].length; i++){
 gemstring += "the " +myGemcolorArray[0][i] +' is '+  myGemColorArray[1][i] +'.';
     };
 gemstring +='gems come in many colors.';
+console.log(gemstring);
