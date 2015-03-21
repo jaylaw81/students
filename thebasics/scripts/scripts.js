8a4e798f26400e77d8b88154c303ed06c0aad349
rhinocoders <info@rhinocoders>
2015-03-21 14:02:56 +0000

    Commit work for natehg24 @ 2015:03:21:14:02:56

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -16,3 +16,4 @@ console.log(myColor);
 var myFruitColorArray = [ ['Apples','Bannanas','Grapes'],  ['Red', 'Yellow', 'Green'] ];
 var myFruitColor = "I like "+myFruitColorArray[0][1]+" because they are "+myFruitColorArray[1][1];
 console.log(myFruitColor);
+var gemString = "Gem colors: ";
