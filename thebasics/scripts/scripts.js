6e1f2048ad160c40d7e83b85009bb927be57bebe
rhinocoders <info@rhinocoders>
2015-03-21 13:57:22 +0000

    Commit work for hermione1217 @ 2015:03:21:13:57:22

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -9,3 +9,4 @@ var count = 10;
 var multiplier = 20;
 var calcVar = "I have "+(count * multiplier)+" friends at 'school'.";
 console.log(calcVar);
+var stringYear = "In the year " + multiplier.toString() + ", most of us were over 3 years old.";
