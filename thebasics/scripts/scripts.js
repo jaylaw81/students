4975de5a74075b529f1c8febbc98e098a0e10c17
rhinocoders <info@rhinocoders>
2015-03-21 13:45:37 +0000

    Commit work for samzissam @ 2015:03:21:13:45:37

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -1,8 +1,8 @@
 var myVariable = "value";
-var stringpartletters = "letters";
-var stringpartnumbers = "numbers";
-var stringpartcharacters = "characters";
-var myFullString = "A string may contain as many "+stringpartletters+", "+stringpartnumbers+" and "+stringpartcharacters+" as you want.";
+var stringpartletters = "eggs";
+var stringpartnumbers = "bacon";
+var stringpartcharacters = "toast";
+var myFullString = "A string may contain as much "+stringparteggs+", "+stringpartbacon+" and "+stringparttoast+" as you want.";
 console.log(myFullString);
 
 
