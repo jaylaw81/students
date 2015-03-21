365970d01e3dc1bc29e9e6ae89c0f7b54257cebb
rhinocoders <info@rhinocoders>
2015-03-21 13:47:21 +0000

    Commit work for hermione1217 @ 2015:03:21:13:47:21

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -2,7 +2,7 @@ var myVariable = "value";
 var stringPartLetters = "reading";
 var stringPartNumbers = "drawing";
 var stringPartCharacters = "writing";
-var myFullString = "Esha loves "+stringPartLetters+", "+stringPartNumbers+", and "+stringPartCharacters+" as you want."; 
+var myFullString = "Esha loves "+stringPartLetters+", "+stringPartNumbers+", and "+stringPartCharacters+" and coding."; 
 console.log(myFullString);
 /* Hi!! I love Harry Potter, drawing, coding, and reading! */
 var count = 10;
