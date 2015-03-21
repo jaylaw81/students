1f0d1c1efa6d1c12b2f1167107d13b56adf53590
rhinocoders <info@rhinocoders>
2015-03-21 17:15:11 +0000

    Commit work for hermione1217 @ 2015:03:21:17:15:11

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -52,5 +52,5 @@ createGallery(galleryContainer, imagePath, imageName, altName);
 function showImage(argument) {
     var img = "<img src='/animals/labrador/" + argument + "'>";
     document.querySelector(".gallery-main-image .image-container").innerHTML = img;
+    return false;
 }
-return false;
