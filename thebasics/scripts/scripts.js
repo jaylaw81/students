935317f37759eab85b016b994842259ae9ef8b30
rhinocoders <info@rhinocoders>
2015-03-21 13:54:22 +0000

    Commit work for natehg24 @ 2015:03:21:13:54:22

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -10,3 +10,4 @@ var calculateVariables = "I have "+(count * multiplier)+" friends on 'facebook'.
 console.log(calculateVariables);
 var stringYear = "In the year " + multiplier.toString() + ", most of us were over 3 years old.";
 console.log(stringYear);
+var myColorArray = ['Orange', 'Red', 'Yellow', 'Green'];
