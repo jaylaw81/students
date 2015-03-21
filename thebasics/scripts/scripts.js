2b1ac6346cd02c241ec1945b747cb8a4f5ccc719
rhinocoders <info@rhinocoders>
2015-03-21 17:13:02 +0000

    Commit work for samzissam @ 2015:03:21:17:13:02

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -53,3 +53,4 @@ var imagePath = "/animals/dolphin1/";
 var imageName = "dolphin";
 var altName = "dolphin";
 createGallery(galleryContainer, imagePath, imageName, altName);
+function showImage(argument) {};
