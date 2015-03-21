30f1bd7b318e13b2b1094185a6784dc6e78d4884
rhinocoders <info@rhinocoders>
2015-03-21 16:57:55 +0000

    Commit work for hermione1217 @ 2015:03:21:16:57:55

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -47,3 +47,4 @@ function createGallery(element,path,file,alt){
 var galleryContainer = ".photo-gallery";
 var imagePath = "/animals/labrador/";
 var imageName = "labrador";
+var altName = "labrador";
