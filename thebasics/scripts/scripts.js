05f8e0c00ccce58903407d7b3a34dbeee1a8f931
rhinocoders <info@rhinocoders>
2015-04-25 18:02:03 +0000

    Commit work for elijah @ 2015:04:25:18:02:03

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -73,6 +73,7 @@ for(var i = 0; i < mygemColorArray[0].length; i++){
     function showImage(argument) {
         var img = "<img src='/animals/owls/" + argument + "'>";
         document.querySelector(".gallery-main-image .image-container").innerHTML = img;
+        return false;
     };
 
 
