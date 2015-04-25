21a445aadc9eb18fa864721f2614c16891434ce8
rhinocoders <info@rhinocoders>
2015-04-25 17:29:32 +0000

    Commit work for sydlovesowls112 @ 2015:04:25:17:29:32

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -62,7 +62,7 @@ function createGallery(element,path,file,alt) {
     for (var j = 1; j <= 8; j++) {
         listring += '<li><a href="#self" onclick="showImage(\'' + file + j + '.jpg\');"><img class="photo" alt="' +alt+ '-'+j+ '" src="' + path + file + j +  '.jpg"></a></li\n'; 
     }
-    document.querySelector(element).innerHTML = listring
+    document.querySelector(element).innerHTML = listring;
 }
 
 
