10933f6e7355ef79db42f1069f3d713010a30d7e
rhinocoders <info@rhinocoders>
2015-04-25 17:39:39 +0000

    Commit work for Isaac1A @ 2015:04:25:17:39:39

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -74,7 +74,7 @@ function createGallery(element,path,file,alt){
 var galleryContainer = ".photo-gallery";
 var imagePath = "/animals/elephants/";
 var imageName = "elaphants";
-
+var imageName = "Elaphant";
 
 
 
