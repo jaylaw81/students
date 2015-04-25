d7c2fa731180bd88e08dfafe9621608445b4a4a1
rhinocoders <info@rhinocoders>
2015-04-25 17:06:03 +0000

    Commit work for sydlovesowls112 @ 2015:04:25:17:06:03

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -57,12 +57,12 @@ function myFirstFunction(_myFriend,_myName,_status) {
 
 myFirstFunction('Mike','Ben','first');
 
-function createGallery(element,path,file,alt){
+function createGallery(element,path,file,alt) {
     var listring = "";
-    for (var j = 1; j <= 8; j++) {}
+    for (var j = 1; j <= 8; j++) {
+        listring += '<li><a href="#self" onclick="showImage(\'' + file + j + '.jpg\');"><img class="photo" alt="' +alt+ '-'+j+ '" src="' + path + file + j +  '.jpg"></a></li\n'; 
+    }
 }
-
-
 
 
 
