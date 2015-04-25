904eb147a30bce9bb242c40e2c95dc1a419c1862
rhinocoders <info@rhinocoders>
2015-04-25 15:11:05 +0000

    Commit work for sydlovesowls112 @ 2015:04:25:15:11:05

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -30,7 +30,11 @@ var gemString = "Gem colors: ";
 
 var myGemColorArray = [ ['Ruby','Shaphire','Emerald'], ['Red','Blue','Green'] ];
 
-for(var i= 0; i< myGemColorArray[0].length; i++){};
+for(var i= 0; i< myGemColorArray[0].length; i++){
+    gemString += "The " + myGemColorArray[0][i] +" is "+ myGemColorArray[1][i] + ". ";    
+};
+
+
 
 
 
