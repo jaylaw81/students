713af1076b2e00d23f06e6bd697214d189a8d8f4
rhinocoders <info@rhinocoders>
2015-04-25 14:33:47 +0000

    Commit work for elijah @ 2015:04:25:14:33:46

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -24,8 +24,8 @@ console.log(myColor);
 
 var myFruitColorArray = [  ['Apples','Bannanas','Grapes'],['Red', 'Yellow', 'Green']  ];
 
-var myFruitColor = "I like "+myFruitColorArray[0][1]+" because they are "+myFruitColorArray[1][1];
-
+var c = "I like "+myFruitColorArray[0][1]+" because they are "+myFruitColorArray[1][1];
+console.log(myColor);
 
 
 
