8dc73df996af9ecad71a8c481dfee8e4b51a96b8
rhinocoders <info@rhinocoders>
2015-04-25 14:03:48 +0000

    Commit work for mattman @ 2015:04:25:14:03:48

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -7,8 +7,8 @@ var stringPartCharacters = "characters";
 var myFullString = "A string may contain as many "+stringPartLetters+", "+stringPartNumbers+" and "+stringPartCharacters+" as you want.";
 console.log(myFullString);
 
-var count = 10;
-var multiplier = 20;
+var count = 1000;
+var multiplier = 2000;
 
 var calculateVariables = "I have "+(count * multiplier)+" friends on 'facebook'.";
 console.log(calculateVariables);
