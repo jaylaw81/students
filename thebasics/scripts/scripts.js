11e797bc04e3307639e12b6856cdcfcd0d588510
rhinocoders <info@rhinocoders>
2015-04-25 14:09:00 +0000

    Commit work for sydlovesowls112 @ 2015:04:25:14:09:00

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -14,4 +14,4 @@ var multiplier = 20;
 var calculateVariables = "I have "+(count * multiplier)+" friends on 'facebook'.";
 console.log(calculateVariables);
 
-var stringYear= "In the year " +mutiplier.toString() + count.toString() + ",most of us were over 3 years old.";
+var stringYear = "In the year " +mutiplier.toString() + count.toString() + ",most of us were over 3 years old.";
