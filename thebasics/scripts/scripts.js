e03df2e127e8976e1ad5f3ce378038f22fd773d9
rhinocoders <info@rhinocoders>
2015-04-25 16:40:15 +0000

    Commit work for Jared @ 2015:04:25:16:40:15

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -57,6 +57,8 @@ console.log(myFriendCompare);
 function myFirstFunction(_myFriend, _myName, _status ) { 
         console.log("Hello "+_myFriend +". It is me, "+ _myName +" and this is my "+ _status +" function!");
     }
+    
+    myFirstFunction('Mike','Ben','first');
 
 
 
