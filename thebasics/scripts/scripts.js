835f3cca3a8587c83bf6ec4d75dab69c7d076acc
rhinocoders <info@rhinocoders>
2015-04-25 14:05:51 +0000

    Commit work for mattman @ 2015:04:25:14:05:51

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -7,11 +7,13 @@ var stringPartCharacters = "characters";
 var myFullString = "A string may contain as many "+stringPartLetters+", "+stringPartNumbers+" and "+stringPartCharacters+" as you want.";
 console.log(myFullString);
 
-var count = 1000;
-var multiplier = 2000;
+var count = 10;
+var multiplier = 20;
 
 var calculateVariables = "I have "+(count * multiplier)+" friends on 'facebook'.";
 console.log(calculateVariables);
+
+var stringYear = "In the year "+multiplier.toString() + count.toString()+", most of us were over 3 years old.";
 
 
 
