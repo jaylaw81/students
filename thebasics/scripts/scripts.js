efa4f202c0d30de2d458b592c1b968b6b672623d
rhinocoders <info@rhinocoders>
2015-04-25 17:38:27 +0000

    Commit work for mattman @ 2015:04:25:17:38:27

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -67,6 +67,7 @@ function createGallery(element, path, file, alt){
 var galleryContainer = ".photo-gallery";
 var imagePath = "/animals/unicorns/";
 var imageName = "unicorn";
+var altName = "unicorn";
 
 
 
