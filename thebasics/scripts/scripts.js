304c3f0355230e34ea86a206048769aaebd57b4e
rhinocoders <info@rhinocoders>
2015-04-25 13:46:07 +0000

    Commit work for izzyparker @ 2015:04:25:13:46:07

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -4,4 +4,4 @@ var stringPartLetters = 'letters';
 var stringPartNumbers = 'numbers';
 var stringPartCharacters = 'characters';
 
-var myFullString = 'A string may contain as many '+stringPartLetters+', '+stringPartNumbers+' and '+stringPartCharacters+' as you want. ';
+var myFullString = 'A string may contain as many '+stringPartLetters+', '+stringPartNumbers+' and '+stringPartCharacters+' as you want."; ';
