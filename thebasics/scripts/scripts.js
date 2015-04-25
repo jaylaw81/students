91deddf9afb793b3db1b8d01f7d4f3021fb0ac16
rhinocoders <info@rhinocoders>
2015-04-25 18:00:45 +0000

    Commit work for Isaac1A @ 2015:04:25:18:00:45

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -81,6 +81,7 @@ createGallery(galleryContainer, imagePath, imageName, altName);
 function showImage(argument) {
     var img = "<img src='/animals/elephants/" + argument + "'>";
     document.querySelector(".gallery-main-image .image-container").innerHTML = img;
+    return false;
 };
 
 
