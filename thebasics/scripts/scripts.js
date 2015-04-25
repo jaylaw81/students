0f2b046ff530c34faa553b3ab35a28b367e55608
rhinocoders <info@rhinocoders>
2015-04-25 14:33:30 +0000

    Commit work for Jared @ 2015:04:25:14:33:30

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -26,7 +26,7 @@ var myFruitColorArray = [ ['Apples','Bannanas','Grapes'], ['Red', 'Yellow', 'Gre
 
 var myFruiColor = "I like "+myFruitColorArray[0][1]+" because they are "+myFruitColorArray[1][1]
 
-
+console.log(myFruitColor);
 
 
 
