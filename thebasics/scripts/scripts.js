a1f102e93ae93d3c8e9997ef4e49aa4a6bdf4dba
rhinocoders <info@rhinocoders>
2015-04-25 16:58:31 +0000

    Commit work for Jared @ 2015:04:25:16:58:31

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -62,7 +62,9 @@ function myFirstFunction(_myFriend, _myName, _status ) {
     
     function createGallery(element,path,file,alt){ 
         var liString = "";
-        for (var j = 1; j <= 8; j++) {}
+        for (var j = 1; j <= 8; j++) {
+       liString += '<li><a href="#self" onclick="showImage(\'' + file + j + '.jpg\');"><img class="photo" alt+"' + alt + '-' + j + '-' + '" src="' + path + file + j+ '.jpg"></a></li\n';
+        }
     }
 
 
