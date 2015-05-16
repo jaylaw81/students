d40990da46536b0ad83d343dbfd76626c0410e33
rhinocoders <info@rhinocoders>
2015-05-16 14:59:09 +0000

    Commit work for alexly123 @ 2015:05:16:14:59:09

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -8,16 +8,22 @@ var count = 10;
 var multiplier = 20;
 var calculateVariables = "I have "+(count * multiplier)+" friends on 'facebook'.";
 console.log(calculateVariables);
+
 var stringYear = "In the year " + multiplier.toString() + count.toString() + ", most of us were over 3 years old.";
 console.log(stringYear);
+
 var myColorArray = ['Orange', 'Red', 'Yellow', 'Green'];
 var myColor = "My favorate color is " + myColorArray[2];
 console.log(myColor);
+
 var myFruitColorArray = [    ['Apples','Bannanas','Grapes'],  ['Red','Yellow', 'Green']  ];
 var myFruitColor = "I like "+myFruitColorArray[0][1]+" because they are "+myFruitColorArray[1][1];
 console.log(myFruitColor);
+
 var gemString = "Gem colors: ";
 var myGemColorArray = [ ['Ruby','Saphire','Emerald'],   ['Red', 'Blue', 'Green']    ];
 for(var i =0; i < myGemColorArray[0].length; i++){ };
 gemString += "The " + myGemColorArray[0][i] +" is "+myGemColorArray[1][i] + ". ";
 gemString += "Gems come in many colors.";
+console.log(gemString);
+
