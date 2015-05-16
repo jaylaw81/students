76f984d40182cb3938fe74404523488521473b2b
rhinocoders <info@rhinocoders>
2015-05-16 16:28:23 +0000

    Commit work for pblasio2 @ 2015:05:16:16:28:23

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -47,8 +47,8 @@ console.log("Hello "+_myFriend +". It is me, "+ _myName +" and this is my "+ _st
 myFirstFunction('Mike','Pat','first');
 
 function createGallery(element,path,file,alt){
-var liString = "";
-}
+    var liString = "";
+    }
 
 
 
