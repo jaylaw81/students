e13ccaabb00dae4ddb0a1cc6124bc834e021191b
rhinocoders <info@rhinocoders>
2015-05-16 14:08:31 +0000

    Commit work for bbroaden @ 2015:05:16:14:08:31

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -7,4 +7,4 @@ console.log(myFullString);
 var count = 10;
 var multiplier = 20; 
 var calculateVariables = "I have "+(count * multiplier)+" friends on 'facebook'.";
-  
+console.log(calculateVariable);
