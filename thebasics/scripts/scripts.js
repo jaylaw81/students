80c708405d6bfbfc977d08166e9e5e61d569c7ee
rhinocoders <info@rhinocoders>
2015-05-16 14:01:44 +0000

    Commit work for bablack5 @ 2015:05:16:14:01:44

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -7,4 +7,4 @@ var stringPartCharacters = "characters";
 var myFullString = "A string may contain as many ";
 myFullString += stringPartLetters+","+stringPartNumbers+" and "+stringPartCharacters+" as you want.";
 
-console.log(myFullString)
+console.log(myFullString);
