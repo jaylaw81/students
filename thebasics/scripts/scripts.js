7b66d954db3d6c7849a7ecf901aa197046934a5e
rhinocoders <info@rhinocoders>
2015-05-16 17:34:58 +0000

    Commit work for manmatt @ 2015:05:16:17:34:58

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -55,6 +55,7 @@ createGallery(galleryContainer, imagePath, imageName, altName);
 function showImage(argument) {
     var img = "<img src='animals/elephants/" + argument+"'>";
     document.querySelector(".gallery-main-image .image-container").innerHTML = img;
+    return false;
 };
 
 
