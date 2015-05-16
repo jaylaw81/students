bb96f0223b0c3726583bafc4f77998f02307c1b4
rhinocoders <info@rhinocoders>
2015-05-16 13:46:11 +0000

    Commit work for pblasio2 @ 2015:05:16:13:46:11

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -2,6 +2,7 @@ var myVariable = "value";
 var stringPartLetters = "letters";
 var stringPartNumbers = "numbers";
 var stringPartCharacters = "characters";
+var myFullString = "A string may contain as many ";
 
 
 
