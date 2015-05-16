055cc3f8ccfcab77a8d0f0683528d9f2761b13b5
rhinocoders <info@rhinocoders>
2015-05-16 16:00:30 +0000

    Commit work for manmatt @ 2015:05:16:16:00:30

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -28,7 +28,9 @@ var myFriendCompare = "";
 var myFriendName = "Mike";
 var myAge = 12;
 var myFriendAge = 13;
-if(myAge > myFriendAge){}else{}
+if(myAge > myFriendAge) {
+    myFriendCompare = "I am older than "+myFriendName+".";
+}else{}
 
 
 
