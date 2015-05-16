e8942c6672f91d5322ee46b7d825f8d57a06934d
rhinocoders <info@rhinocoders>
2015-05-16 13:54:20 +0000

    Commit work for Coen1888 @ 2015:05:16:13:54:20

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -3,7 +3,7 @@ var stringPartLetters = "letters";
 var stringPartNumbers = "numbers";
 var stringPartCharacters = "characters";
 var myFullString = "A string may contain as many";
-myFullString += ","+stringPartLetters+" and "+stringPartNumbers+" and "+sringPartChartracters+" a you want.";
+myFullString += ","+stringPartLetters+" and "+stringPartNumbers+" and "+sringPartCharacterss+" a you want.";
 console.log(myFullString)
 
 
