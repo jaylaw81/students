abae5375a9868d1fe58b87cd28064e705b8e2008
rhinocoders <info@rhinocoders>
2015-05-16 17:03:44 +0000

    Commit work for 20charlie02 @ 2015:05:16:17:03:44

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -44,4 +44,4 @@ function createGallery(element,path,file,alt){
         document.querySelector(element).innerHTML = liString;
     }
 var galleryContainer = ".photo-gallery";
-var imagePath = "/animals/germanshepherd/german-shepherd2.jpg"
+var imagePath = "/animals/germanshepherd/"
