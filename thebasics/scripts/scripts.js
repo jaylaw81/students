98c65049618a15b354394374599a3f1804141afd
rhinocoders <info@rhinocoders>
2015-05-16 14:49:05 +0000

    Commit work for bablack5 @ 2015:05:16:14:49:05

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -24,7 +24,6 @@ var myColor = "My favorite color is " + myColorArray[3];
 console.log(myColor);
 
 var myFruitColorArray = [ ["Apples", "Bananas","Grapes"], ["Red", "Yellow", "Green"] ];
-var myFruitColorArray = "I like " + myFruitColorArray[0][1]+ " because they are " + myFruitColorArray[1][1];
-console.log(myFruitColorArray);
+var myFruitColor = "I like " + myFruitColorArray[0][1]+ " because they are " + myFruitColorArray[1][1];
 console.log(myFruitColor);
 
