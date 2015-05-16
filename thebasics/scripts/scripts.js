c003614ca61fb9d2d0263b4a6735fd2f2987432f
rhinocoders <info@rhinocoders>
2015-05-16 14:02:33 +0000

    Commit work for bbroaden @ 2015:05:16:14:02:33

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -2,7 +2,7 @@ var myVariable = "value";
 var stringPartLetters = "letters";
 var stringPartNumers = "numbers" ;
 var stringPartCharacters = "Characters";
-var myFullString =  "A string may contain as many "+stringPartLetter+", "+stringPartNumbers+" and "+stringPartCharacters+" as you want.";
+var myFullString =  "A string may contain as many "+stringPartLetters+", "+stringPartNumbers+" and "+stringPartCharacters+" as you want.";
 console.log(myFullString);
 var count = 10;
 var multiplier = 20; 
