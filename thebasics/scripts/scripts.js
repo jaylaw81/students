a1c40ff875e5dcd74179befdd43bae5b91830f32
rhinocoders <info@rhinocoders>
2015-05-16 15:07:18 +0000

    Commit work for 20charlie02 @ 2015:05:16:15:07:18

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -11,7 +11,7 @@ console.log(calculateVariables);
 var stringYear = "In the year " + multiplier.toString() + count.toString() + ", most of us were over 3 years old.";
 console.log(stringYear);
 var myColorArray = ['orange', 'red', 'yellow', 'green',];
-var myColor = "My favorite " + myColorArray[2];
+var myColor = "My favorite color is " + myColorArray[2];
 console.log(myColor);
 var myFruitColorArray = [ ['Apples', 'Bannanas', 'Grapes'],  ['Red', 'Yellow', 'Green'] ];
 var myFruitColor = "I like "+myFruitColorArray[0][1]+" because they are "+myFruitColorArray[1][1];
