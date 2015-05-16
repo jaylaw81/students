8f1dbaf33ba3daf0728c60c3a7ea371154252277
rhinocoders <info@rhinocoders>
2015-05-16 14:00:50 +0000

    Commit work for Coen1888 @ 2015:05:16:14:00:50

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -5,7 +5,8 @@ var stringPartCharacters = "characters";
 var myFullString = "A string may contain as many";
 myFullString += ","+stringPartLetters+" and "+stringPartNumbers+" and "+stringPartCharacters+" a you want.";
 console.log(myFullString)
-
+var count = 10;
+var multiplier = 20;
 
 
 
