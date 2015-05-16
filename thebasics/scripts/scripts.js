e60736f705e62572a55a89420fbce47692c7351f
rhinocoders <info@rhinocoders>
2015-05-16 14:00:42 +0000

    Commit work for 20charlie02 @ 2015:05:16:14:00:42

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -4,3 +4,6 @@ var stringPartNumbers = "123";
 var stringPartCharacters = "abc 123";
 var myFullString = "A string mar contain as many "+stringPartLetters+", "+stringPartNumbers+" and "+stringPartCharacters+" as you want.";
 console.log(myFullString);
+var count = 10;
+var multiple = 20;
+
