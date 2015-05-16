c2840657be9273f20a7a5f26b87636e06ea83008
rhinocoders <info@rhinocoders>
2015-05-16 15:09:40 +0000

    Commit work for chrispycoder @ 2015:05:16:15:09:40

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -43,3 +43,49 @@ for(var i = 0; i < myGemColorArray[0].length; i++){
 gemString += "Gems come in many colors";
 
 console.log(gemString);
+
+var myFriendCompare = "";
+var myFriendName = "Mike";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
