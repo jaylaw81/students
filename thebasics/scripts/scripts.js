8a52b095b5db46246677e1de822e5a2c0c9456d1
rhinocoders <info@rhinocoders>
2015-05-16 13:59:42 +0000

    Commit work for chrispycoder @ 2015:05:16:13:59:42

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -4,7 +4,7 @@ var stringPartLetters = "letters";
 var stringPartNumbers = "numbers";
 var stringPartCharacters = "characters";
 
-var myFullString = " A string may contain as many "+stringPartLetters+", "+stringPartNumbers+" and "+stringPartCharacters+" as you want.";                                                                                          aaaaaaaa
+var myFullString = " A string may contain as many "+stringPartLetters+", "+stringPartNumbers+" and "+stringPartCharacters+" as you want.";  
 
 console.log(myFullString);
 
@@ -13,3 +13,4 @@ var multiplier = 20;
 
 var calculateVariables = "I have "+(count * multiplier)+" friends on 'facebook'.";
 
+console.log(calculateVariables);
