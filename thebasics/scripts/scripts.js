e5334a5a8e9e3747cd228e7674b558d9e0193c73
rhinocoders <info@rhinocoders>
2015-05-16 15:16:00 +0000

    Commit work for bablack5 @ 2015:05:16:15:16:00

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -45,6 +45,7 @@ if(myAge > myFriendAge){
     myFriendCompare = "I am older than " + myFriendName+ ". "
     } 
         else { 
+            myFriendCompare = myFriendName+ " is older than me.";
         }
     
 
