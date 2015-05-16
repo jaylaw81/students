a3b5c69a83221cd20277d4b74858814732d83ba6
rhinocoders <info@rhinocoders>
2015-05-16 14:25:28 +0000

    Commit work for pblasio2 @ 2015:05:16:14:25:28

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -3,7 +3,6 @@ var stringPartLetters = "letters";
 var stringPartNumbers = "numbers";
 var stringPartCharacters = "characters";
 var myFullString = "A string may contain as many "+stringPartLetters+", "+stringPartNumbers+" and "+stringPartCharacters+" as you want.";
-
 console.log(myFullString);
 
 var count = 10;
@@ -12,11 +11,8 @@ var multiplier = 20;
 var calculateVariables = "I have "+(count * multiplier)+" friends on 'facebook'.";  
 console.log(calculateVariables);
 var stringYear = "In the year " + multiplier.toString() + count.toString() + ", most of us were 3 years old.";
-
 console.log(stringYear);
 
-var myColorArray = ['Orange', 'Red', 'Yellow', 'Green'];
-
+var myColorArray = ['Orange', 'Red', 'Blue', 'Green'];
 var myColor = "My favorite color is " + myColorArray[2];
-
 console.log(myColor);
