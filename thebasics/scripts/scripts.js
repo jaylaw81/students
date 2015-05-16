5e5fe101d7465da38ace9490783d4bd3a629b6d4
rhinocoders <info@rhinocoders>
2015-05-16 14:21:14 +0000

    Commit work for bablack5 @ 2015:05:16:14:21:14

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -20,5 +20,5 @@ console.log(stringYear);
 
 var myColorArray = ["Orange", "Red", "Yellow", "Green"];
 
-var myColor = "My favorite color is " + myColorArray[2];
+var myColor = "My favorite color is " + myColorArray[3];
 
