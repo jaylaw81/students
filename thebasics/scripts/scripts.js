af125669c98edeb65a0baeb3a4283d15a5fa4c8e
rhinocoders <info@rhinocoders>
2015-05-16 15:00:34 +0000

    Commit work for alexly123 @ 2015:05:16:15:00:34

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -4,6 +4,7 @@ var stringPartLetters = "numbers";
 var stringPartCharicters = "characters";
 var myFullString = "A string may contain as many "+stringPartLetters+", "+stringPartNumbers+", and "+stringPartCharacters+" as you want.";
 console.log(myFullString);
+
 var count = 10;
 var multiplier = 20;
 var calculateVariables = "I have "+(count * multiplier)+" friends on 'facebook'.";
@@ -27,3 +28,5 @@ gemString += "The " + myGemColorArray[0][i] +" is "+myGemColorArray[1][i] + ". "
 gemString += "Gems come in many colors.";
 console.log(gemString);
 
+var myFriendCompare = "";
+var myFriendCName = "Mike";
