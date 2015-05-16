a132dda8450a49e69ab59ade2ee0252d7811ca86
rhinocoders <info@rhinocoders>
2015-05-16 15:04:53 +0000

    Commit work for 20charlie02 @ 2015:05:16:15:04:53

--- thebasics/scripts/scripts.js
+++ thebasics/scripts/scripts.js
@@ -5,7 +5,7 @@ var stringPartCharacters = "abc 123";
 var myFullString = "A string mar contain as many "+stringPartLetters+", "+stringPartNumbers+" and "+stringPartCharacters+" as you want.";
 console.log(myFullString);
 var count = 10;
-var multiple = 20;
+var multiplier = 20;
 var calculateVariables = "I have "+(count * multiplier)+" friends on 'facebook'.";
 console.log(calculateVariables);
 var stringYear = "In the year " + multiplier.toString() + count.toString() + ", most of us were over 3 years old.";
